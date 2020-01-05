3a5bf835bebfebc1b4aeeba01c9a2c18e8ebfa48
Vikas Bhavsar <vikasbhvsr@gmail.com>
2020-01-05 13:06:37 -0800

    added joke search functionality

--- js/script.js
+++ js/script.js
@@ -1,29 +1,70 @@
-const uri = 'https://icanhazdadjoke.com';
-const loading = document.querySelector('#loading');
+const inputValue = document.querySelector('#search-joke');
+let jokes = document.querySelector('.jokes');
 
-let header = new Headers();
-header.append('Accept', 'application/json');
-let request = new Request(uri, {
-    method: 'GET',
-    headers: header,
-    mode: 'cors'
+const searchButton = document.querySelector('[type="submit"]');
+searchButton.addEventListener('click', (event) => {
+    jokes.innerHTML = '';
+    event.preventDefault();
+    searchDadJokes();
 });
 
+function searchDadJokes() {
+    const uri = `https://icanhazdadjoke.com/search?term=${inputValue.value}`;
+    const loading = document.querySelector('#loading');
+    let header = new Headers();
+    header.append('Accept', 'application/json');
+    let request = new Request(uri, {
+        method: 'GET',
+        headers: header,
+        mode: 'cors'
+    });
+    loading.removeAttribute('hidden');
+    fetch(request)
+    .then(res=>res.json())
+    .then(data => {
+        data.results.forEach(result => {
+            const dadJoke = document.querySelector('.jokes');
+            const emojis = ['😂','😝', '🤣','😹','🙈','😆','🙄','🤪'];
+            const randomEmoji = emojis[Math.floor(Math.random()*emojis.length)];
+            let jokeHTML = `
+                <div class="joke">
+                    <h2 id${result.id}>${result.joke} ${randomEmoji}</h2>
+                </div>
+            `;
+            dadJoke.insertAdjacentHTML('afterbegin', jokeHTML);
+        });
+        loading.setAttribute('hidden', '');
+    })
+    .catch(err=>console.error(err));
+}
+
 function fetchDadJokes() {
+    const uri = 'https://icanhazdadjoke.com';
+    const loading = document.querySelector('#loading');
+    
+    let header = new Headers();
+    header.append('Accept', 'application/json');
+    let request = new Request(uri, {
+        method: 'GET',
+        headers: header,
+        mode: 'cors'
+    });
     loading.removeAttribute('hidden');
     fetch(request)
     .then(res=>res.json())
     .then(data => {
         loading.setAttribute('hidden', '');
-        const emojis = ['😂', '🤣','😹','🙈','😆'];
+        const {id, joke} = data;
+        const emojis = ['😂','😝', '🤣','😹','🙈','😆','🙄','🤪'];
         const randomEmoji = emojis[Math.floor(Math.random()*emojis.length)];
-        const dadJoke = document.querySelector('.dad-joke');
+        const dadJoke = document.querySelector('.jokes');
         const dadJokeHTML = `
-        <h2 id=${data.id}>${data.joke} ${randomEmoji}</h2>
+        <div class="joke">
+        <h2 id=${id}>${joke} ${randomEmoji}</h2>
+        </div>
         `;
         dadJoke.insertAdjacentHTML('afterbegin', dadJokeHTML);
     })
     .catch(err=>console.error(err));
 }
-
 fetchDadJokes();
