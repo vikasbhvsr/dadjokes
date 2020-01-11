a82940776ce02b3aba152d49437cb2ec4ec20233
Vikas Bhavsar <vikasbhvsr@gmail.com>
2020-01-11 15:08:37 -0800

    random joke button and max width set

--- js/script.js
+++ js/script.js
@@ -1,75 +1,79 @@
 const searchField = document.querySelector('#search-joke');
 let jokes = document.querySelector('.jokes');
+let randomJoke = document.querySelector('.random-joke');
 
 // const searchButton = document.querySelector('[type="submit"]')
 
-searchField.addEventListener('keyup', (event) => {
-    event.preventDefault();
-    jokes.innerHTML = '';
-    searchDadJokes();
+searchField.addEventListener('keyup', event => {
+  event.preventDefault();
+  jokes.innerHTML = '';
+  searchDadJokes();
 });
 
-
 function getRandomEmojis() {
-    const emojis = ['😂','😝', '🤣','😹','🙈','😆','🙄','🤪'];
-    return randomEmoji = emojis[Math.floor(Math.random()*emojis.length)];
+  const emojis = ['😂', '😝', '🤣', '😹', '🙈', '😆', '🙄', '🤪'];
+  return (randomEmoji = emojis[Math.floor(Math.random() * emojis.length)]);
 }
 
-
 function searchDadJokes() {
-    const uri = `https://icanhazdadjoke.com/search?term=${searchField.value}`;
-    const loading = document.querySelector('#loading');
-    let header = new Headers();
-    header.append('Accept', 'application/json');
-    let request = new Request(uri, {
-        method: 'GET',
-        headers: header,
-        mode: 'cors'
-    });
-    loading.removeAttribute('hidden');
-    fetch(request)
-    .then(res=>res.json())
+  const uri = `https://icanhazdadjoke.com/search?term=${searchField.value}`;
+  const loading = document.querySelector('#loading');
+  let header = new Headers();
+  header.append('Accept', 'application/json');
+  let request = new Request(uri, {
+    method: 'GET',
+    headers: header,
+    mode: 'cors'
+  });
+  loading.removeAttribute('hidden');
+  fetch(request)
+    .then(res => res.json())
     .then(data => {
-        data.results.forEach(result => {
-            const dadJoke = document.querySelector('.jokes');
-            const {id, joke} = result;
-            let jokeHTML = `
+      data.results.forEach(result => {
+        const dadJoke = document.querySelector('.jokes');
+        const { id, joke } = result;
+        let jokeHTML = `
                 <div class="joke">
                     <h2 id=${id}>${joke} ${getRandomEmojis()}</h2>
                 </div>
             `;
-            dadJoke.insertAdjacentHTML('afterbegin', jokeHTML);
-        });
-        loading.setAttribute('hidden', '');
+        dadJoke.insertAdjacentHTML('afterbegin', jokeHTML);
+      });
+      loading.setAttribute('hidden', '');
     })
-    .catch(err=>console.error(err));
+    .catch(err => console.error(err));
 }
 
 function fetchDadJokes() {
-    const uri = 'https://icanhazdadjoke.com';
-    const loading = document.querySelector('#loading');
-    
-    let header = new Headers();
-    header.append('Accept', 'application/json');
-    let request = new Request(uri, {
-        method: 'GET',
-        headers: header,
-        mode: 'cors'
-    });
-    loading.removeAttribute('hidden');
-    fetch(request)
-    .then(res=>res.json())
+  const uri = 'https://icanhazdadjoke.com';
+  const loading = document.querySelector('#loading');
+
+  let header = new Headers();
+  header.append('Accept', 'application/json');
+  let request = new Request(uri, {
+    method: 'GET',
+    headers: header,
+    mode: 'cors'
+  });
+  loading.removeAttribute('hidden');
+  fetch(request)
+    .then(res => res.json())
     .then(data => {
-        loading.setAttribute('hidden', '');
-        const {id, joke} = data;
-        const dadJoke = document.querySelector('.jokes');
-        const dadJokeHTML = `
+      loading.setAttribute('hidden', '');
+      const { id, joke } = data;
+      const dadJoke = document.querySelector('.jokes');
+      const dadJokeHTML = `
         <div class="joke">
         <h2 id=${id}>${joke} ${getRandomEmojis()}</h2>
         </div>
         `;
-        dadJoke.insertAdjacentHTML('afterbegin', dadJokeHTML);
+      dadJoke.insertAdjacentHTML('afterbegin', dadJokeHTML);
     })
-    .catch(err=>console.error(err));
+    .catch(err => console.error(err));
 }
-fetchDadJokes();
+window.addEventListener('DOMContentLoaded', fetchDadJokes);
+randomJoke.addEventListener('click', e => {
+  event.preventDefault();
+  jokes.innerHTML = '';
+  fetchDadJokes();
+});
